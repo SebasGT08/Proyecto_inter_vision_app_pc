e63e326953c4013fb4851a91c689df75fc844bd9
SebasGT08 <guaman.sebastian802gmail.com>
2024-05-29 01:24:11 -0500

    Trackbars

--- app_pc.cpp
+++ app_pc.cpp
@@ -9,25 +9,39 @@
 using namespace std;
 using namespace cv;
 
-// Función para cambiar el color de las gafas
+void eventoTrack(int valor, void *data) {}
+
+// Funcion para cambiar el color de las gafas
 void cambiarColorGafas(Mat& imagen, const Mat& mascara, const Scalar& nuevoColor) {
-    // Crear una imagen del mismo tamaño que la imagen original con el nuevo color
+    // Crear una imagen del mismo tamano que la imagen original con el nuevo color
     Mat gafasColor = Mat::zeros(imagen.size(), imagen.type());
     gafasColor.setTo(nuevoColor, mascara);
 
-    // Aplicar la máscara inversa a la imagen original para mantener los otros colores
+    // Aplicar la mascara inversa a la imagen original para mantener los otros colores
     Mat imagenFondo;
     bitwise_and(imagen, imagen, imagenFondo, ~mascara);
 
-    // Combinar las dos imágenes
+    // Combinar las dos imagenes
     bitwise_or(gafasColor, imagenFondo, imagen);
 }
 
-int main(int argc, char* argv[]) {
-    // Manejo de Video
-    VideoCapture videoPrincipal("http://192.168.83.169:5000/video_feed");
+// Función para detección de bordes usando Canny
+void deteccionBordes(const Mat &imagen, Mat &bordes) {
+    Mat imagen_gris;
+    cvtColor(imagen, imagen_gris, COLOR_BGR2GRAY);
+    Canny(imagen_gris, bordes, 50, 150);
+    // cvtColor(bordes, bordes, COLOR_GRAY2BGR); // Convertir a BGR para que coincida el tipo
+}
 
-    // Verificamos si el stream de video se pudo abrir
+int valor_b = 0;
+int valor_g = 0;
+int valor_r = 255;
+
+int main(int argc, char* argv[]) {
+    // Capturar Video
+    VideoCapture videoPrincipal("http://192.168.18.250:5000/video_feed");
+
+    // Verificamos si el stream se puede abrir
     if (!videoPrincipal.isOpened()) {
         cerr << "Error al abrir el stream de video!" << endl;
         return -1;
@@ -35,47 +49,57 @@ int main(int argc, char* argv[]) {
 
     // Crear ventanas para mostrar los resultados
     namedWindow("Original", WINDOW_AUTOSIZE);
+    namedWindow("Gafas Bordes", WINDOW_AUTOSIZE);
     namedWindow("Gafas Color Cambiado", WINDOW_AUTOSIZE);
     namedWindow("Gafas Detectadas", WINDOW_AUTOSIZE);
 
-    Mat frameColor;
+    // Crear trackbars para elegir el color de las gafas (se cambia el nombre de BRG A RGB por que se tranforma depues)
+    createTrackbar("R", "Gafas Color Cambiado", &valor_b, 255, eventoTrack, NULL);
+    createTrackbar("G", "Gafas Color Cambiado", &valor_g, 255, eventoTrack, NULL);
+    createTrackbar("B", "Gafas Color Cambiado", &valor_r, 255, eventoTrack, NULL);
+
+    Mat frameOriginal;
     Mat imgHSV;
     Mat mascara;
 
     // Valores HSV para detectar el color magenta
     int hmin = 140, hmax = 170, smin = 100, smax = 255, vmin = 150, vmax = 255;
 
-    // Color al que se cambiarán las gafas (rojo en este caso, en espacio BGR)
-    Scalar nuevoColor(0, 0, 255);
-
     // Bucle principal
     while (true) {
-        videoPrincipal >> frameColor; // Capturar frame del video principal
-        if (frameColor.empty()) break; // Salir si no hay más frames
+        videoPrincipal >> frameOriginal; // Capturar frame del video principal
+        if (frameOriginal.empty()) break; // Salir si no hay más frames
 
         // Convertir a HSV
-        cvtColor(frameColor, imgHSV, COLOR_BGR2HSV);
+        cvtColor(frameOriginal, imgHSV, COLOR_BGR2HSV);
 
         // Crear una máscara basada en el rango HSV
         inRange(imgHSV, Scalar(hmin, smin, vmin), Scalar(hmax, smax, vmax), mascara);
 
+        // Actualizar el color de las gafas usando los valores de los trackbars
+        Scalar nuevoColor(valor_b, valor_g, valor_r);
+
         // Cambiar el color de las gafas en la imagen
-        Mat gafasColorCambiado = frameColor.clone();
+        Mat gafasColorCambiado = frameOriginal.clone();
         cambiarColorGafas(gafasColorCambiado, mascara, nuevoColor);
 
         // Extraer las gafas detectadas en una imagen separada
-        Mat gafasDetectadas = Mat::zeros(frameColor.size(), frameColor.type());
-        frameColor.copyTo(gafasDetectadas, mascara);
+        Mat gafasDetectadas = Mat::zeros(frameOriginal.size(), frameOriginal.type());
+        frameOriginal.copyTo(gafasDetectadas, mascara);
 
-        // Convertir gafasColorCambiado y gafasDetectadas de vuelta a BGR para mostrar correctamente
-        Mat gafasColorCambiadoBGR, gafasDetectadasBGR;
-        cvtColor(gafasColorCambiado, gafasColorCambiadoBGR, COLOR_BGR2RGBA);
-        cvtColor(gafasDetectadas, gafasDetectadasBGR, COLOR_BGR2RGBA);
+        //Deteccion de bordes
+        Mat bordes_canny;
+        deteccionBordes(gafasDetectadas, bordes_canny);
+
+        //Convertir a RGB
+        Mat gafasColorCambiadoRGB;
+        cvtColor(gafasColorCambiado, gafasColorCambiadoRGB, COLOR_BGR2RGBA);
 
         // Mostrar las imágenes en las ventanas
-        imshow("Original", frameColor);
-        imshow("Gafas Color Cambiado", gafasColorCambiadoBGR);
-        imshow("Gafas Detectadas", gafasDetectadasBGR);
+        imshow("Original", frameOriginal);
+        imshow("Gafas Bordes", bordes_canny);
+        imshow("Gafas Color Cambiado", gafasColorCambiadoRGB);
+        imshow("Gafas Detectadas", gafasDetectadas);
 
         // Salir si se presiona la tecla ESC
         if (waitKey(1) == 27) break;
